809fbfef2164029945b3c6079904e6124cdd61a8
grim <75869731+vys69@users.noreply.github.com>
2025-01-28 04:21:13 -0800

    universal typing

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+export interface Store {
+    id: string;
+    userId: string;
+    groupId: string;
+    name: string;
+    slug: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface Product {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+}
