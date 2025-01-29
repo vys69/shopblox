0c19c4ebd467e99270970c6a2894c3c7c168fc2b
grim <75869731+vys69@users.noreply.github.com>
2025-01-29 00:49:13 -0800

    add header css variable

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-black text-zinc-500 lowercase">
+      <body className="bg-black text-zinc-500 lowercase h-screen" style={{ '--header-height': '88px' } as React.CSSProperties}>
         <Navigation/>
-        <div className="min-h-screen min-w-screen flex items-center justify-center">
-          <main className="min-w-full min-h-full max-w-md flex flex-col items-center justify-center">{children}</main>
+        <div className="min-w-screen flex items-center justify-center">
+          <main className="min-w-screen flex flex-col items-center">{children}</main>
         </div>
       </body>
     </html>
